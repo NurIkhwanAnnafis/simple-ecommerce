e06f9f1b81a1e9ffb3e00c35a5cf70c73e22de19
NurIkhwanAnnafis <nur.ikhwan@majoo.id>
2022-01-08 10:38:22 +0700

    add: style sign in

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 import './App.css';
 
@@ -14,6 +15,7 @@ function App () {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/shop" element={<ShopPage />} />
+          <Route path="/signin" element={<SignInAndSignUp />} />
         </Routes>
       </div>
   )
