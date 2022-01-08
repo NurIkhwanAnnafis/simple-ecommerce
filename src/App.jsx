379f7abda74bca8d860e395349b029cf43f35d02
NurIkhwanAnnafis <nur.ikhwan@majoo.id>
2022-01-08 09:46:05 +0700

    update: pages shop page,component collection preview and collection item

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,16 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom'
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 import './App.css';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App () {
   return (
     <div>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/shop/hats" element={<HatsPage />} />
+          <Route path="/shop" element={<ShopPage />} />
         </Routes>
       </div>
   )
